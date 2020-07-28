cf84e311c633559e1fc5f5b1ea9274246696e912
Adam Richie-Halford <richford@uw.edu>
2020-07-28 08:00:55 -0700

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # cloud_bids_layout documentation build configuration file, created by
 # sphinx-quickstart on Fri Jun  9 13:47:02 2017.
@@ -12,14 +13,23 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import alabaster
+import os
+import sys
+
+# General information about the project
+project = "Cloud-BIDS-Layout"
+copyright = "2020, Adam Richie-Halford"
+author = "Adam Richie-Halford"
+
+currentdir = os.path.abspath(os.path.dirname(__file__))
+
+sys.path.append(os.path.join(currentdir, "tools"))
+
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
-#
-import os
-import sys
-
 sys.path.insert(0, os.path.abspath(".."))
 
 import cloud_bids_layout
@@ -27,12 +37,26 @@ import cloud_bids_layout
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
+needs_sphinx = "1.0"  # numpydoc requires sphinx >= 1.0
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+sys.path.append(os.path.abspath("sphinxext"))
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "alabaster",
+    "github",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -46,14 +70,30 @@ source_suffix = ".rst"
 # The master toctree document.
 master_doc = "index"
 
-# General information about the project.
-project = "Cloud-BIDS-Layout"
-copyright = "2020, Adam Richie-Halford"
-author = "Adam Richie-Halford"
+# --- Sphinx Gallery ---
+sphinx_gallery_conf = {
+    # path to your examples scripts
+    "examples_dirs": "../examples",
+    # path where to save gallery generated examples
+    "gallery_dirs": "auto_examples",
+    # To auto-generate example sections in the API
+    "doc_module": ("cloudknot",),
+    # Auto-generated mini-galleries go here
+    "backreferences_dir": "gen_api",
+}
 
-# The version info for the project you're documenting, acts as replacement
-# for |version| and |release|, also used in various other places throughout
-# the built documents.
+# Automatically generate stub pages for API
+autoclass_content = "both"  # include both class docstring and __init__
+autodoc_default_flags = ["members", "inherited-members"]
+autosummary_generate = True
+
+# Napoleon settings (other than default)
+napoleon_google_docstring = False
+napoleon_use_rtype = False
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
 #
 # The short X.Y version.
 version = cloud_bids_layout.__version__
@@ -83,20 +123,34 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+html_theme_options = {
+    "description": "Use pybids with datasets in the cloud",
+    "github_user": "nrdg",
+    "github_repo": "cloud_bids_layout",
+    "github_type": "star",
+    "show_powered_by": True,
+    "fixed_sidebar": True,
+}
+
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = [alabaster.get_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+# Custom sidebar templates, maps document names to template names.
+html_sidebars = {"**": ["about.html", "navigation.html", "searchbox.html"]}
+
+# If false, no module index is generated.
+# html_domain_indices = True
+html_domain_indices = False
 
 # -- Options for HTMLHelp output ---------------------------------------
 
@@ -156,7 +210,7 @@ texinfo_documents = [
         "Cloud-BIDS-Layout Documentation",
         author,
         "cloud_bids_layout",
-        "One line description of project.",
+        "Use pybids with datasets in the cloud.",
         "Miscellaneous",
     )
 ]
--- docs/index.rst
+++ docs/index.rst
@@ -1,20 +1,107 @@
 Welcome to Cloud-BIDS-Layout's documentation!
-======================================
+=============================================
+
+Cloud-BIDS-Layout is a lightweight wrapper for pybids' BIDS.Layout that
+can grab BIDS studies from Amazon S3.
+
+Usage
+-----
+
+.. code-block:: python
+
+   # Insert an amazing Cloud-BIDS-Layout example here
+   pass
+
+Motivation
+----------
+
+The [Brain Imaging Data Structure (BIDS)](https://bids.neuroimaging.io/)
+is an intuitive, accessible, and community-driven data specification for
+neuroimaging data. [Pybids](https://bids-standard.github.io/pybids/)
+is an exceptionally written Python library that makes it easy for
+researchers to query, summarize, and manipulate BIDS-compliant data.
+However, it's workhorse `BIDSLayout` class doesn't index remote datasets
+stored in the cloud. That's where Cloud-BIDS-Layout comes in.
+
+Cloud-BIDS-Layout allows its user to specify a remote location for
+a BIDS-Compliant dataset. Currently only Amazon S3 locations are
+supported but support for Google Cloud Storage and others is coming
+soon. Cloud-BIDS-Layout creates a lightweight semblance of the remote
+dataset, just enough to pass to pybids' `BIDSLayout` for indexing.
+The user can then use pybids' familiar `.get()` method to select a
+subset of the study that they wish to download to the host. See the
+[documentation](https://nrdg.github.io/cloud_bids_layout) for more
+details.
+
+`cottoncandy <https://doi.org/10.5281/zenodo.1034342>`_ for interacting with
+numpy array data on `Amazon S3 <https://aws.amazon.com/s3/>`_. Or see `pywren
+<http://pywren.io/>`_, which enables users to run their existing python code
+on `AWS Lambda <https://aws.amazon.com/lambda/>`_, providing convenient
+distributed execution for jobs that fall within the AWS Lambda limits (maximum
+300 seconds of execution time, 1.5 GB of RAM, 512 MB of local storage, and no
+root access). For jobs that require more, we introduce cloudknot to execute
+existing python code on AWS Batch.
+
+Cloudknot takes as input a python function, Dockerizes it for use in an Amazon
+ECS instance, and creates all the necessary AWS Batch constituent resources to
+submit jobs. You can then use cloudknot to submit and view jobs for a range
+of inputs.
+
+Installation and getting started
+--------------------------------
+
+To install Cloud-BIDS-Layout and see usage details
+visit :ref:`installation-label` and :ref:`usage-label`.
+
+Documentation and API
+---------------------
+
+Most users will only need to interact with the `CloudBIDSLayout` class. See
+:ref:`usage-label` for more details.
+
+Bugs and issues
+---------------
+
+If you are having issues, please let us know by `opening up a new issue
+<https://github.com/nrdg/cloud_bids_layout/issues>`_.
+You will probably want to tag your issue with the "bug" or "question" label.
+
+Contribute
+----------
+
+We invite you to contribute to Cloud-BIDS-Layout. Take a look at the `source code
+<https://github.com/nrdg/cloud_bids_layout>`_. Or tackle one of the `open issues
+<https://github.com/nrdg/cloud_bids_layout/issues>`_. Issues labeled "help wanted"
+or "good first issue" are particularly appropriate for beginners.
+
+License
+-------
+
+The project is licensed under the `MIT license
+<https://github.com/nrdg/cloud_bids_layout/blob/master/LICENSE>`_.
+
+Acknowledgements
+----------------
+
+The development of Cloud-BIDS-Layout is supported through grant
+`1RF1MH121868-01 <https://projectreporter.nih.gov/project_info_details.cfm?aid=9886761&icde=46874320&ddparam=&ddvalue=&ddsub=&cr=2&csb=default&cs=ASC&pball=>`_
+from the `National Institutes for Mental Health <https://www.nimh.nih.gov/index.shtml>`_/`The BRAIN
+Initiative <https://braininitiative.nih.gov/>`_.
+
+We are also grateful for support from the `Gordon and Betty Moore
+Foundation <https://www.moore.org/>`_ and the `Alfred P. Sloan Foundation <https://sloan.org.>`_ to the `University of
+Washington eScience Institute <http://escience.washington.edu/>`_ Data Science Environment, as well as
+support from the `Washington Research Foundation <http://www.wrfseattle.org/>`_ to eScience and to the
+`University of Washington Institute for Neuroengineering <http://uwin.washington.edu/>`_.
 
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+   :hidden:
 
-   readme
-   installation
-   usage
+   installation <installation>
+   usage <usage>
+   faq <faq>
+   examples <https://github.com/nrdg/cloud_bids_layout/tree/master/examples>
    modules
-   contributing
-   authors
-   history
-
-Indices and tables
-==================
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+   contributing <https://github.com/nrdg/cloud_bids_layout/blob/master/CONTRIBUTING.md>
+   code <https://github.com/nrdg/cloud_bids_layout>
+   bugs <https://github.com/nrdg/cloud_bids_layout/issues>
